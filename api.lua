1a4ee7435f4fa1c15a67bc5c3c3084f0e61972bd
nevertrollsh <143024241+nevertrollsh@users.noreply.github.com>
2023-11-08 20:04:48 +0530

    Update api.lua
    Hex Patch Support!

--- api.lua
+++ api.lua
@@ -20,7 +20,12 @@ nt_version = "2.0"
     
   end
 
+
+
+
+  patches= gg.makeRequest("https://raw.githubusercontent.com/nevertrollsh/nt-gg-api/main/patches.lua").content
   mainapi = gg.makeRequest("https://raw.githubusercontent.com/nevertrollsh/nt-gg-api/main/main.lua").content
   dbapi = gg.makeRequest("https://raw.githubusercontent.com/nevertrollsh/nt-gg-api/main/ntdatabase.lua").content
   pcall(load(mainapi))
   pcall(load(dbapi))
+  pcall(load(patches))
