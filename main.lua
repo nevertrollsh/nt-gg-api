24c9cdfe9ab609c98aad897c7195c3ed08bd2a6b
nevertrollsh <143024241+nevertrollsh@users.noreply.github.com>
2023-11-08 13:31:39 +0530

    Update main.lua
    Error Fix #4

--- main.lua
+++ main.lua
@@ -8,21 +8,9 @@ double = 64
 qword = 32
 default_results = 100
 
-local function isArr(tbl)
- 
-    if type(tbl) == "table" then
-        local length = #tbl
-        for i = 1, length do
-            if tbl[i] == nil then
-                return false
-            end
-        end
-        return true
-    else
-        return false
-    end
+
   
-end
+
 
 function search(value, type)
  gg.searchNumber(value, type)
