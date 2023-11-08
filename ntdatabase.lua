5691742f4e84325a150a04d52c4a501854e79786
nevertrollsh <143024241+nevertrollsh@users.noreply.github.com>
2023-11-08 14:49:24 +0530

    Create ntdatabase.lua
    Added ntdatabase support!

---
+++ ntdatabase.lua
@@ -0,0 +1,54 @@
+db = {}
+
+if db.storage == nil then db.storage = "database.dat" end
+
+local file_path = "database.dat"
+
+-- Check if the file exists
+local file = io.open(file_path, "r")
+
+if file then
+    -- The file exists, so close it
+    file:close()
+else
+    -- The file doesn't exist, create it
+    file = io.open(file_path, "w")
+    if file then
+        file:close()
+        print("File created successfully.")
+    else
+        print("Failed to create the file.")
+    end
+end
+
+
+
+function db.save(varName, value)
+    local fileName = db.storage
+    
+    -- Open the file in read mode
+    local file = io.open(fileName, "r")
+
+    if file then
+        local data = file:read("*all")
+        file:close()
+
+        -- Check if the variable already exists
+        local pattern = varName .. " = \"[^\"]+\""
+
+        if not data:match(pattern) then
+            -- If the variable doesn't exist, add it to the end of the file with double quotes
+            data = data .. "\n" .. varName .. " = \"" .. value .. "\""
+        else
+            -- If the variable exists, find and replace the old value
+            data = data:gsub(pattern, varName .. " = \"" .. value .. "\"")
+        end
+
+        -- Open the file again in write mode to overwrite it
+        local file = io.open(fileName, "w")
+        file:write(data)
+        file:close()
+    else
+        print("Failed to open the file for reading.")
+    end
+end
