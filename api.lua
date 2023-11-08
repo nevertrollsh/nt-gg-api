d17f894f8bb2451331ca67abf17ed95c4ad7f81c
nevertrollsh <143024241+nevertrollsh@users.noreply.github.com>
2023-11-08 12:00:31 +0530

    Create api.lua
    Added Main API.

---
+++ api.lua
@@ -0,0 +1,20 @@
+nt = {}
+
+libraries = {"math.lua","random.lua","strings.lua'}
+
+  for i,v in pairs(libraries) do
+    
+    if v == "math.lua" then
+
+    end
+
+    if v == "random.lua" then
+    
+    end
+
+    if v == "strings.lua" then
+
+    end
+
+    
+  end
