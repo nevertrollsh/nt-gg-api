13db8b271cd5732d1fee177fa27e17239fcc997b
nevertrollsh <143024241+nevertrollsh@users.noreply.github.com>
2023-11-09 18:25:47 +0530

    Update api.lua
    Added import() to avoid unnecessary packages.

--- api.lua
+++ api.lua
@@ -1,14 +1,35 @@
 nt = {}
-
-libraries = {"math.lua","random.lua","strings.lua"}
 nt_version = "2.0"
+mainapi = gg.makeRequest("https://raw.githubusercontent.com/nevertrollsh/nt-gg-api/main/main.lua").content
 
+libs = {"ntdatabase","random","patches"}
 
+function loadContent(cont)
+ local loading = gg.makeRequest(cont).content
+ pcall(load(loading))
+end
 
-
-  patches= gg.makeRequest("https://raw.githubusercontent.com/nevertrollsh/nt-gg-api/main/patches.lua").content
-  mainapi = gg.makeRequest("https://raw.githubusercontent.com/nevertrollsh/nt-gg-api/main/main.lua").content
-  dbapi = gg.makeRequest("https://raw.githubusercontent.com/nevertrollsh/nt-gg-api/main/ntdatabase.lua").content
-  pcall(load(mainapi))
-  pcall(load(dbapi))
-  pcall(load(patches))
+function import(pkg)
+ for i,v in pairs(libs) do
+ 
+ if v == "ntdatabase" then
+ loadContent("https://raw.githubusercontent.com/nevertrollsh/nt-gg-api/main/ntdatabase.lua")
+ print("Loaded package: 'ntdatabase'\n")
+ end
+ 
+ if v == "random" then
+  
+  print("Library random was not loaded due to undeveloped code.")
+ end
+ 
+ if v == "patches" then
+ loadContent("https://raw.githubusercontent.com/nevertrollsh/nt-gg-api/main/patches.lua")
+ print("Loaded package: 'patches'\n")
+ end
+ 
+ 
+ 
+ 
+ end
+end
+  
